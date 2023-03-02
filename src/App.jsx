d43aeb9b405de9a15249e225915d44c7579f4a59
Pedro Gontijo <70620630+PedroGontijo13@users.noreply.github.com>
2023-03-01 22:11:34 -0800

    Passing from child to parents

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,18 @@ import Card from "./components/Card";
 import jokesData from "./data/jokesData";
 
 function App() {
+
+  const f = (id) => {
+    console.log(id)
+  }
+
   return (
     <div className="App">
       <div className="container">
         <div className="row">
           <div className="col-12">
             {jokesData.map((item, index) => (
-              <Card data={item} key={index} />
+              <Card data={item} func={f} id={index} key={index} />
             ))}
           </div>
         </div>
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,13 @@
-export default function Card(props) {
+export default function Card(props, {index}) {
   const { data } = props;
+  console.log(props)
   return (
     <div className="card" style={{margin: '5px'}}>
       <div className="card-body">
-        <h5 className="card-title">Joke</h5>
+        <h5 className="card-title">Joke{index}</h5>
         <h6 className="card-subtitle mb-2 text-muted">{data.setup}</h6>
         <p className="card-text">{data.punchLine}</p>
+        <button onClick={() => props.func(6)}>LAUGTH</button>
       </div>
     </div>
   );
